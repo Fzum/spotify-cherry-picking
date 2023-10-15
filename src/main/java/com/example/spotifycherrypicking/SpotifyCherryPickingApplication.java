c486a34cafa1052eba12df248aa6a77f9accc367
fabian_sellemond <sellemondfabian@gmail.com>
2023-10-15 19:26:06 +0200

    add mvp working

--- src/main/java/com/example/spotifycherrypicking/SpotifyCherryPickingApplication.java
+++ src/main/java/com/example/spotifycherrypicking/SpotifyCherryPickingApplication.java
@@ -1,13 +1,64 @@
 package com.example.spotifycherrypicking;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@EnableWebSecurity
 public class SpotifyCherryPickingApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(SpotifyCherryPickingApplication.class, args);
     }
 
+}
+
+@Configuration
+class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeHttpRequests(authorize -> authorize.anyRequest().authenticated())
+                .oauth2Login(oAuth2LoginConfigurer -> oAuth2LoginConfigurer.failureUrl("/fail"));
+
+        return http.build();
+    }
+}
+
+@RestController
+class SpotifyController {
+
+    private final SpotifyService spotifyService;
+
+    @Autowired
+    public SpotifyController(SpotifyService spotifyService) {
+        this.spotifyService = spotifyService;
+    }
+
+    @GetMapping("/playlists")
+    public String getPlaylists() {
+        return spotifyService.fetchPlaylists();
+    }
+}
+
+@Service
+class SpotifyService {
+
+    public String fetchPlaylists() {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        System.out.println(authentication);
+        return authentication.getName();
+    }
 }
